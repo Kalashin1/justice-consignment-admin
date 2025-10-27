a78c243c4d0538cea1dab95819ec205edd006b8a
Kinanee Samson <kinaneesamsonjohn@gmail.com>
2025-10-27 01:03:52 +0100

    Update update-shipment-form.jsx

--- src/pages/dashboard/update-shipment/components/update-shipment-form.jsx
+++ src/pages/dashboard/update-shipment/components/update-shipment-form.jsx
@@ -169,6 +169,7 @@ const UpdateShipmentForm = () => {
                           <option value="canceled">Canceled</option>
                           <option value="delivered">Delivered</option>
                           <option value="dispensed">Dispensed</option>
+                          <option value="held">Held</option>
                         </select>
                       </div>
                     </div>
